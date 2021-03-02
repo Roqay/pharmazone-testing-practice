8b17058ce90e7cf355ac7361acacd6b2bad61890
asma203 <eng_asma@live.com>
2021-03-02 17:44:34 +0200

    login

--- MR/src/main/java/pharmazone/MR/LoginPage.java
+++ MR/src/main/java/pharmazone/MR/LoginPage.java
@@ -1,11 +1,22 @@
 package pharmazone.MR;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends PageBase{
 
 	public LoginPage(WebDriver driver) {
 		super(driver);
 	}
+	@FindBy(name="email")
+	WebElement emailtxt;
+	
+	@FindBy(name="password")
+	WebElement psswdtxt;
+	
+	
+	@FindBy(partialLinkText ="login")
+	WebElement loginbtn;
 
 }
