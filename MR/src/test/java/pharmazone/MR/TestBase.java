2e2f28d69ae647f06fd8adabf1e5c50fe9b9bfb4
asma203 <eng_asma@live.com>
2021-03-02 15:02:43 +0200

    initial TestBase Setup

---
+++ MR/src/test/java/pharmazone/MR/TestBase.java
@@ -0,0 +1,69 @@
+package pharmazone.MR;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.asserts.SoftAssert;
+
+import com.applitools.eyes.selenium.Eyes;
+
+public class TestBase {
+	protected static WebDriver driver;
+	protected static Eyes eyes;
+	private static Properties prop;
+
+	// soft assertion method
+	SoftAssert softassert = new SoftAssert();
+
+	// Initiate the Chrome browser
+	@BeforeMethod
+	public void openBrowser() throws InterruptedException, Exception {
+		prop = new Properties();
+		try {
+			FileInputStream fis = new FileInputStream("resources\\test.properties");
+			prop.load(fis);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		String chromeDriverPath = prop.getProperty("chromepath");
+		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+	//	System.setProperty("webdriver.chrome.driver", "resources\\ChromeDriver\\chromedriver1.exe");
+		driver = new ChromeDriver();
+		driver.navigate().to("https://pharmazone.roqay.solutions/login");
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		initiEyes();
+	}
+
+	// Initiate Eye apptlitool for visual testing
+	private static void initiEyes() {
+		eyes = new Eyes();
+		// eyes.setApiKey("XaK9REdNw7MmCWtMu8xNndXETBeretnveKH2V104PT107mA110");
+		eyes.setApiKey(prop.getProperty("api"));
+
+	}
+
+	// validate window design and elements
+	public void validateWindow() {
+		eyes.open(driver, "Uni", Thread.currentThread().getStackTrace()[2].getMethodName());
+		//get image for the whole screen
+		eyes.setForceFullPageScreenshot(true);
+		eyes.checkWindow();
+		eyes.close();
+	}
+
+	@AfterMethod
+	public static void quitBrowser() {
+		driver.quit();
+		eyes.abortIfNotClosed();
+	}
+
+
+}
