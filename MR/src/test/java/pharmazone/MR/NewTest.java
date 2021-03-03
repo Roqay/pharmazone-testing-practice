5ca2bfdfedf15bf99b3a594d365e9d440141fb9b
amr moahmed <Amr.aboShosha@roqay.com.kw>
2021-03-03 22:14:09 +0200

    Add FireFox Driver

---
+++ MR/src/test/java/pharmazone/MR/NewTest.java
@@ -0,0 +1,11 @@
+package pharmazone.MR;
+
+import org.testng.annotations.Test;
+
+public class NewTest extends TestBase {
+  @Test
+  public void f() {
+	  
+	  driver.navigate().to("https://web.whatsapp.com/");
+  }
+}
